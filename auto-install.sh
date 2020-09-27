0ecfa8bb5a29614e248993c0278c159cbcd916e1
liruizhi0871 <1023976839@qq.com>
2020-09-27 22:38:39 +0000

    My oh-my-zsh

--- auto-install.sh
+++ auto-install.sh
@@ -1,4 +1,3 @@
-#https://gitee.com/liruizhi/oh-my-zsh/blob/master/auto-install-and-update.sh
 git clone https://gitee.com/liruizhi/oh-my-zsh.git ~/.oh-my-zsh
 cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
 chsh -s /bin/zsh
