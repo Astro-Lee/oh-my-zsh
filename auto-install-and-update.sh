52ff52c8a60e2930641e6066355debcc21308bb0
liruizhi <1023976839@qq.com>
2020-09-27 19:24:42 +0800

    update auto-install-and-update.sh.

--- auto-install-and-update.sh
+++ auto-install-and-update.sh
@@ -1,5 +1,4 @@
-#需要用系统命令先安装zsh和git.
-git clone https://gitee.com/liruizhi/oh-my-zsh.git ~/.oh-my-zsh   #把oh-my-zsh克隆下来
-cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc    #构建.zshrc
-chsh -s /bin/zsh   #把Bash替换成zsh
-source ~/.zshrc   #运行~/.zshrc
+git clone https://gitee.com/liruizhi/oh-my-zsh.git ~/.oh-my-zsh
+cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
+chsh -s /bin/zsh
+source ~/.zshrc
